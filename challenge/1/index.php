f6a7ac185ec61eed213053877421373b16b9eaae
dotlineX <59593757+dotlineX@users.noreply.github.com>
2022-11-14 14:35:28 +0100

    check if user is logged in

--- challenge/1/index.php
+++ challenge/1/index.php
@@ -1,4 +1,36 @@
 <?php
+
+// Check if vertx cookie is present
+if (isset($_COOKIE['vertx-web_session'])) {
+    // request data from api
+    $ch = curl_init();
+    $url = 'http://localhost:8080/user';
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_COOKIE, 'vertx-web.session='.$_COOKIE['vertx-web_session']);
+
+    // get response
+    $response = curl_exec($ch);
+    curl_close($ch);
+    $response_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    if ($response_code != 200) {
+        header('Location: /login.html');
+    }
+} else {
+    header('Location: /login.html');
+}
+
+
+$msg = ($_GET['msg']);
+
+if (isset($msg)) {
+    if ($msg == "<img src='' onerror=alert(1)>" || $msg == '<img src="" onerror=alert(1)>') {
+        $flag = 'FLAG{XSS-IS-EASY-PEASY-LEMON-SQEEZY}';
+    } else if ($msg) {
+        $msg = htmlspecialchars($msg);
+    }
+}
+
 error_reporting(0);
 $msg = '';
 $flag = '';
