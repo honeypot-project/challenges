3fdfe76498be1dd76995cc2da138792b33345fe3
dotlineX <59593757+dotlineX@users.noreply.github.com>
2022-11-15 17:45:01 +0100

    merge

--- assets/js/config.js
+++
@@ -1,4 +0,0 @@
-_config = {
-    getAPIUrl: function() { return `http://64.225.68.102/api`;},
-    defaultProfileImage: function () { return `https://media.discordapp.net/attachments/1022109268897448019/1038449726179246201/image0.png`;}
-}
