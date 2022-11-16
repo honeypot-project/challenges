21a62aefe287e9f7d0d23cfb305cb40a57d9473e
migueldb1 <102661224+migueldb1@users.noreply.github.com>
2022-11-16 17:05:01 +0100

    changed doors

--- index.php
+++ index.php
@@ -16,14 +16,14 @@ checkLogin();
 <body>
 
 <div id="main-container">
-    <img src="images/doors-background.png" usemap="#image-map" alt="doors-challenges">
+    <img src="images/doors-background.jpg" usemap="#image-map" alt="doors-challenges">
 
     <map name="image-map">
-        <area target="_blank" alt="1" title="1" href="challenge/1" coords="86,299,266,662" shape="rect">
-        <area target="_blank" alt="2" title="2" href="challenge/2" coords="408,299,590,662" shape="rect">
-        <area target="_blank" alt="3" title="3" href="challenge/3" coords="732,299,915,661" shape="rect">
-        <area target="_blank" alt="4" title="4" href="challenge/4" coords="1051,297,1231,662" shape="rect">
-        <area target="_blank" alt="5" title="5" href="challenge/5" coords="1378,299,1561,664" shape="rect">
+        <area target="_blank" alt="1" title="1" href="challenge/1" coords="71,232,209,516" shape="rect">
+        <area target="_blank" alt="2" title="2" href="challenge/2" coords="320,238,456,512" shape="rect">
+        <area target="_blank" alt="3" title="3" href="challenge/3" coords="577,239,711,518" shape="rect">
+        <area target="_blank" alt="4" title="4" href="challenge/4" coords="817,231,963,515" shape="rect">
+        <area target="_blank" alt="5" title="5" href="challenge/5" coords="1074,236,1218,518" shape="rect">
     </map>
 </div>
 
